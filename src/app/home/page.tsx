ab4e511af45aa1bfb3abc8b9d21b221e8e64b552
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-08-03 21:38:06 -0300

    feat: import header component and add styles to my main div

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,10 @@
+import Header from "./layout/header";
+
 export default function Home() {
   return (
-    <div>
+    <div className="w-full h-full min-h-screen bg-[#F8F2E1]">
+      <Header />
+
       <h1>My loved Things HOME</h1>
     </div>
   );
