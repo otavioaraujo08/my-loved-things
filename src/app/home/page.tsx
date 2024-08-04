1071e56f9e5dd9722f7a75c918ea47121abed134
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-08-03 22:13:50 -0300

    update: home page styles and tags

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,35 @@
+import Image from "next/image";
 import Header from "./layout/header";
 
 export default function Home() {
   return (
-    <div className="w-full h-full min-h-screen bg-[#F8F2E1]">
+    <div className="flex flex-col items-center gap-40 w-full h-full min-h-screen bg-[#F8F2E1]">
       <Header />
 
-      <h1>My loved Things HOME</h1>
+      <div className="flex flex-row items-center">
+        <Image
+          src="/images/humamuruHappy.png"
+          alt="happy image mood"
+          width={250}
+          height={250}
+        />
+
+        <div className="text-center">
+          <h1 className="text-[3rem] lg:text-[4rem] xl:text-[6rem] font-bold font-notoSansKorean tracking-widest">
+            Mood
+          </h1>
+          <h1 className="text-[2.5rem] lg:text-[3.5rem] xl:text-[5.5rem] font-bold font-notoSansKorean tracking-widest">
+            Diário
+          </h1>
+        </div>
+
+        <Image
+          src="/images/humamuruPlaying.png"
+          alt="Joy image mood"
+          width={250}
+          height={250}
+        />
+      </div>
     </div>
   );
 }
